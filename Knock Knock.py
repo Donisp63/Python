c146ac460f23e9fa208ad4d0b509d4557fce4bed
Pedro E. Donis <pdonis@students.mchenry.edu>
2024-09-29 22:13:38 -0500

    Add files via upload

---
+++ Knock Knock.py
@@ -0,0 +1,9 @@
+#This is one line of code using the escape characters
+print("Knock Knock\nWho's there?\nDewey\nDewey who?\nDewey have to keep telling knock knock jokes.\n")
+
+#This is multiple line of print functions to write the joke
+print("Knock Knock")
+print("Who's there?")
+print("Dewey")
+print("Dewey who?")
+print("Dewey have to keep telling knock knock jokes.")
