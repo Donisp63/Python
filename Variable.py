696127d8efb9d8bbf8b2058c1ae3f44aafcd5de3
Pedro E. Donis <pdonis@students.mchenry.edu>
2024-09-29 22:30:08 -0500

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 69
+print("Literal:", x)
+
+# Integer
+y = -50
+print("Integer:", y)
+
+# Float
+z = 3.15
+print("Float:", z)
+
+# String
+name = "Pedro"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
