6e45ce13a870a844ff7dc30d6b5ecca099f5ffaf
Pedro E. Donis <pdonis@students.mchenry.edu>
2024-09-29 22:48:09 -0500

    Add files via upload

---
+++ Mathmatical Operators.py
@@ -0,0 +1,6 @@
+length = 10 # The length of the rectangle
+width = 20 # The width of a the rectangle
+area = length * width # The formula for the area of a rectangle.
+
+# printing "The area of the rectangle with a length of <length> and a width of <width> is <area>."
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area) +".")
