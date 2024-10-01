85ae30c37e131734fbe8d627c928c212d806b0d5
Pedro E. Donis <pdonis@students.mchenry.edu>
2024-09-30 19:16:06 -0500

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,4 @@
+tank_size = 15 # Tank size in gallons.
+distance = 600 # How far the vehicle can go with a full tank.
+mpg = distance / tank_size # Formula for finding miles per gallon.
+print("Miles per gallon:", mpg) # Should print Miles per gallon: <mpg>
