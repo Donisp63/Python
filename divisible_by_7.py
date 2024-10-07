efa8738b73975a2bb51c76c8af53a8a869cf565b
Pedro E. Donis <pdonis@students.mchenry.edu>
2024-10-06 21:31:03 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,4 @@
+# For loop that goes through each number in a range of 1-300
+for number in range(1,301):
+    if number % 7 == 0: # if that number is divisible by 7 print that number
+        print(number)
